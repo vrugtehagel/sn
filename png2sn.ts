9052bb63899332faa9ab0b5080f678ef4a85c4c6
vrugtehagel <vrugtehagel@pm.me>
2025-11-04 17:48:47 +0100

    Rework png2sn: better compression & more generic
    Removed the anti-alias-removing parts, and the palette size option.
    Instead, the palette is automatically generated by the colors in the
    image, with a slight sensitivity to colors that are very close together
    (in which case it corrects towards the most common color).
    
    The algorithm now also optimizes the palette order, since different
    palette orders result in different compression levels. There are also
    slight performance improvements, although the palette ordering logic
    probably overshadows that.

--- png2sn.ts
+++ png2sn.ts
@@ -5,126 +5,42 @@ type Color = number
 
 /** Converts an array of bytes representing a PNG into an array of bytes
  * representing a (hopefully smaller) SN file. */
-export async function convert(
-	input: Uint8Array,
-	options: {paletteSize?: number} = {},
-): Uint8Array {
+export async function convert(input: Uint8Array): Uint8Array {
 	const jimp = await Jimp.fromBuffer(input.buffer, 'image/png')
 	const {width, height, data} = jimp.bitmap
 	const tones = new Uint32Array(data.buffer)
-	const palette = getPalette(tones, options.paletteSize)
-
-	/** Safely retrieve the color of a pixel in the image, returning `null` if
-	 * the given (x, y) position lies outside the image. */
-	function get(x: number, y: number): Color | null {
-		if(x < 0 || x >= width) return null
-		if(y < 0 || y >= height) return null
-		return tones[y * width + x]
+	const rootPalette = getPalette(tones, 6)
+	let best
+	for(const defaultColor of rootPalette){
+		const palette = rootPalette.filter(color => color != defaultColor)
+		palette.unshift(defaultColor)
+		const orderedPalette = orderPalette(tones, palette, width, height)
+		const bytes = compress(tones, orderedPalette, width, height)
+		if(best?.length <= bytes.length) continue
+		best = bytes
 	}
+	return best
+}
 
-	/** Retrieve a color of a pixel in the image, where coordinates wrap around
-	 * the image if too close to the side. If, even with wrapping, the position
-	 * lies outside the image boundaries (i.e. too close to the top or bottom)
-	 * then the default color is returned (the first color in the palette). */
-	function at(x: number, y: number): Color {
-		const index = y * width + x
-		return tones[index] ?? palette[0]
-	}
-
-	// The input image can very well contain pixels of colors that are not in
-	// the palette. That's fine, but we need to "snap" them to colors in our
-	// palette. Unfortunately this is harder to do than just "closest color",
-	// because anti-aliasing is a thing. Drawing a curve, in black, with a
-	// palette that should also include gray, will cause some of the pixels on
-	// the edge of the curve to snap to gray incorrectly.
-
-	// To fix this, we "detect" anti-aliasing. We do this by finding one-pixel
-	// wide lines of any length, and snapping those not to the colors in the
-	// palette but the surrounding pixels (but only surrounding pixels that are
-	// also in the palette).
-	function isAntiAliasing(x: number, y: number): boolean {
-		const color = get(x, y)
-		const isHorizontal = get(x + 1, y) == color
-		const [dx, dy] = isHorizontal ? [1, 0] : [0, 1]
-		do {
-			if(get(x + dy, y + dx) == color) return false
-			if(get(x - dy, y - dx) == color) return false
-		} while(get(x += dx, y += dy) == color)
-		return true
-	}
-
-	/** Find the nearest color in the palette by Euclidean distance. */
-	function getClosestColor(color: Color, pool: Color[] = palette): Color {
-		if(pool.includes(color)) return color
-		const distances = pool.map(from => colorDistance(from, color))
-		const closest = Math.min(...distances)
-		const index = distances.indexOf(closest)
-		return pool[index]
-	}
-
-	// First, we do a pass where every pixel that doesn't qualify as
-	// anti-aliasing gets snapped to a color in the palette (by distance). Then
-	// we handle anti-aliasing. This is because the AA snapping relies on
-	// nearby pixels being in the palette.
-	for(let y = 0; y < height; y++) for(let x = 0; x < width; x++){
-		if(isAntiAliasing(x, y)) continue
-		tones[y * width + x] = getClosestColor(get(x, y))
-	}
-
-	// Next, we loop through and treat the remaining pixels as anti-aliasing.
-	// Snap to the nearest (closest) color that is also in the palette.
-	for(let y = 0; y < height; y++) for(let x = 0; x < width; x++){
-		const color = get(x, y)
-		if(palette.includes(color)) continue
-		const candidates = new Set()
-		for(let r = 1; r <= 3 && candidates.size == 0; r++){
-			for(let dx = -r; dx <= r; dx++) for(let dy = -r; dy <= r; dy++){
-				if(dx < r && dx >= -r && dy < r && dy > -r) continue
-				const candidate = get(x + dx, y + dy)
-				if(palette.includes(candidate)) candidates.add(candidate)
-			}
-		}
-		if(candidates.size == 0){
-			for(const color of palette) candidates.add(color)
-		}
-		tones[y * width + x] = getClosestColor(color, [...candidates])
-	}
-
-	// Now on to actually doing the compression work. We construct an array of
-	// bits, which is inefficient, but easy to do.
+/** Do the actual compression given a palette in a fixed order. Returns the SN-
+ * formatted file as a byte array. */
+function compress(
+	tones: Uint32Array,
+	palette: Color[],
+	width: number,
+	height: number,
+): Uint8Array {
 	const bits = []
 	let zeroes = 0
 	for(let y = 0; y < height; y++) for(let x = 0; x < width; x++){
-		const correct = at(x, y) ?? palette[0]
-		const left = at(x - 1, y) ?? palette[0]
-		const top = at(x, y - 1) ?? palette[0]
-		const topleft = at(x - 1, y - 1) ?? palette[0]
-		const topright = at(x + 1, y - 1) ?? palette[0]
-		const guess = (() => {
-			if(left == top){
-				if(left != topleft) return top
-				if(left == topright) return top
-				if(at(x + 2, y - 1) != topright) return top
-				if(at(x + 1, y - 2) != top) return top
-				if(at(x + 2, y - 3) != top) return top
-				return topright
-			} else if(left == topleft){
-				if(at(x - 1, y - 2) != top) return top
-				if(at(x - 2, y - 1) != left) return top
-				if(at(x - 2, y - 3) != top) return top
-				return left
-			} else if(top == topleft){
-				if(at(x - 2, y - 1) != left) return left
-				if(at(x - 1, y - 2) != top) return left
-				if(at(x - 3, y - 2) != left) return left
-				return top
-			} else return top
-		})()
+		const at = (dx, dy) => tones[(y + dy) * width + x + dx] ?? palette[0]
+		const correct = at(0, 0)
+		const guess = getBestGuess(at)
 		if(guess == correct){
 			zeroes++
 			continue
 		}
-		const contextual = new Set([guess, top, left, topleft, topright])
+		const contextual = [guess, at(-1, 0), at(0, -1), at(-1, -1), at(1, -1)]
 		const guesses = [...new Set([...contextual, ...palette])]
 		let badness = guesses.indexOf(correct)
 		if(zeroes > 0){
@@ -136,64 +52,117 @@ export async function convert(
 		zeroes = badness == palette.length - 1 ? 0 : 1
 		while(badness--) bits.push(1)
 	}
-
-	// Complete the byte
-	while(bits.length % 8 > 0) bits.push(0)
-
-	// Now our "bits" array is ready, time to build the SN file.
-	const start = 16 + 4 * palette.length
-	const filesize = start + (bits.length / 8)
-	const sn = new Uint8Array(filesize)
-	sn.set([
-		0x53, 0x4e, 0x78, 0x56, 0x48, 0x30, 0x2e, 0x31, // "SNxVH0.1"
+	const start = 16 + palette.length * 4
+	const size = start + Math.ceil(bits.length / 8)
+	const bytes = new Uint8Array(size)
+	bytes.set([
+		0x53, 0x4e, 0x78, 0x56, 0x48, 0x30, 0x2e, 0x31,
 		width & 0xFF, width >>> 8, height & 0xFF, height >>> 8,
-		0x00, 0x00, 0x00, // Currently reserved for future extensions
+		0x00, 0x00, 0x00,
 		palette.length,
-		...palette.flatMap(color => getRgba(color)),
+		...palette.flatMap(color => toRgba(color)),
 	], 0)
 	for(let index = 0; index < bits.length; index += 8){
 		let byte = 0
-		for(let offset = 0; offset < 8; offset++){
-			byte <<= 1
-			byte += bits[index + offset]
+		for(let b = 0; b < 8; b++) byte += (bits[index + b] << (7 - b))
+		bytes[start + (index >>> 3)] = byte
+	}
+	return bytes
+}
+
+/** The guessing function, given a tone array and an `at()` function that
+ * returns a color at an (x, y) position. Only returns the best guess.
+ * Note that that `at()` function also incorporates the default color in the
+ * palette. */
+function getBestGuess(at){
+	const left = at(-1, 0)
+	const top = at(0, -1)
+	const topleft = at(-1, -1)
+	const topright = at(1, -1)
+	if(left == top){
+		if(left != topleft) return top
+		if(left == topright) return top
+		if(at(2, -1) != topright) return top
+		if(at(1, -2) != top) return top
+		if(at(2, -3) != top) return top
+		return topright
+	} else if(left == topleft){
+		if(at(-1, -2) != top) return top
+		if(at(-2, -1) != left) return top
+		if(at(-2, -3) != top) return top
+		return left
+	} else if(top == topleft){
+		if(at(-2, -1) != left) return left
+		if(at(-1, -2) != top) return left
+		if(at(-3, -2) != left) return left
+		return top
+	} else return top
+}
+
+/** For a given palette, order the colors to optimize output filesize. The
+ * first color in the palette, the default color, is not reordered, because its
+ * effects are unpredictable. For the other colors, we can count how often we
+ * end up guessing them wrong, and therefore sort them without having to try
+ * every single order. */
+function orderPalette(
+	tones: Uint32Array,
+	palette: Color[],
+	width: number,
+	height: number,
+): Color[] {
+	const badGuesses = new Map(palette.map(color => [color, 0]))
+	for(let y = 0; y < height; y++) for(let x = 0; x < width; x++){
+		const at = (dx, dy) => tones[(y + dy) * width + x + dx] ?? palette[0]
+		const color = at(0, 0)
+		if(color == at(-1, 0)) continue
+		if(color == at(-1, -1)) continue
+		if(color == at(0, -1)) continue
+		if(color == at(1, -1)) continue
+		badGuesses.set(color, (badGuesses.get(color) ?? 0) + 1)
+	}
+	badGuesses.set(palette[0], Infinity)
+	return getKeysByValuesDesc(badGuesses)
+}
+
+/** Get the most common colors from the image. This'll be the palette. The
+ * threshold determines how close two colors need to be in order for them to
+ * be considered the same color. */
+function getPalette(tones: Uint32Array, threshold: number): Color[] {
+	const counts = new Map<Color, number>()
+	for(const tone of tones) counts.set(tone, (counts.get(tone) ?? 0) + 1)
+	const colors = getKeysByValuesDesc(counts)
+	const palette = []
+	while(colors.length > 0){
+		const target = colors.shift()
+		palette.push(target)
+		for(const color of [...colors]){
+			if(colorDistance(target, color) > threshold) continue
+			const index = colors.indexOf(color)
+			colors.splice(index, 1)
 		}
-		sn[start + (index >>> 3)] = byte
 	}
-
-	return sn
-}
-
-/** Get the most common colors from the image. This'll be the palette. If no
- * palette size is specified, all colors are used (probably undesirable). */
-function getPalette(tones: Uint32Array, size?: number): Color[] {
-	const counts = new Map()
-	for(const tone of tones){
-		const count = counts.get(tone) ?? 0
-		counts.set(tone, count + 1)
-	}
-	const desc = quantify => (a, b) => quantify(b) - quantify(a)
-	const entries = [...counts]
-	entries.sort(desc(([color, count]) => count))
-	const colors = entries.map(([color]) => color)
-	if(!size) return colors
-	return colors.slice(0, size)
-}
-
-/** Extract R, G, B, A values from a single 32-bit integer representing a
- * color. */
-function getRgba(color: Color): [number, number, number, number] {
-	const rgba = []
-	while(rgba.length < 4){
-		rgba.push(color & 0xFF)
-		color >>>= 8
-	}
-	return rgba
+	return palette
 }
 
 /** Calculate the Euclidean distance between two colors. Note; this doesn't
  * match how similar colors actually are, visually, very well. */
 function colorDistance(from: Color, to: Color): number {
-	const [r1, g1, b1, a1] = getRgba(from)
-	const [r2, g2, b2, a2] = getRgba(to)
+	const [r1, g1, b1, a1] = toRgba(from)
+	const [r2, g2, b2, a2] = toRgba(to)
 	return Math.hypot(r2 - r1, g2 - g1, b2 - b1, a2 - a1)
+}
+
+/** Extract R, G, B, A values from a single 32-bit integer representing a
+ * color. */
+function toRgba(color: Color): [number, number, number, number] {
+	const rgba = []
+	while(rgba.push(color & 0xFF) < 4) color >>>= 8
+	return rgba
+}
+
+/** Get the keys of a map in descending order based on their values. */
+function getKeysByValuesDesc<T>(map: Map<T, number>): T[] {
+	const entries = [...map]
+	entries.sort((one, other) => other[1] - one[1])
+	return entries.map(([key]) => key)
 }
